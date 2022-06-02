d99c99e6404b98bedc38518fe1e489a433c25501
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2022-06-02 01:48:41 -0400

    New web template: progress in migration to Xranklin.

--- config.md
+++ config.md
@@ -6,4 +6,11 @@ Add here global page variables to use throughout your website.
 author = "JuliaParallel"
 mintoclevel = 2
 
+# Author badges
+using DelimitedFiles
+author_badges_mat = readdlm("_assets/author-badges.dat", ':')
+author_badges = Dict()
+[author_badges[author_badges_mat[i]] = rstrip.(lstrip.(split(author_badges_mat[i,2], ",")))
+ for i in 1:size(author_badges_mat,1) ]
+
 +++
--- utils.jl
+++ utils.jl
@@ -6,11 +6,26 @@ using Dates
 Plug in the list of news contained in the `/news/` folder.
 """
 function hfun_news()
-    curyear = year(Dates.today())
+    author_badges = globvar("author_badges")
+
+    # Changing page width in "News" section 
     io = IOBuffer()
-    for year in curyear:-1:2021
+    write(io, """<style>
+                    @media (min-width: 1024px) {
+                      .franklin-content {
+                         width: 100%;
+                         padding-left: 0%;
+                         padding-right: 0%;
+                      }
+                    }
+                 </style>""")
+    
+    # Add news
+    curyear = year(Dates.today())
+    write(io, """<div class="news-container">""")
+    for year in curyear:-1:2022
         ys = "$year"
-        year < curyear && write(io, "\n**$year**\n")
+        year < curyear
         for month in 12:-1:1
             ms = "0"^(month < 10) * "$month"
             base = joinpath("news", ys, ms)
@@ -22,9 +37,11 @@ function hfun_news()
                 ps  = splitext(post)[1]
                 url = "/news/$ys/$ms/$ps/"
                 surl = strip(url, '/')
-                title = getvarfrom(:title, surl, default=nothing)
-                title === nothing && (title = "Untitled")
-                pubdate = getvarfrom(:published, surl, default=nothing)
+                post_title = pagevar(surl, :post_title)
+                short_descr = pagevar(surl, :short_descr)
+                post_author = pagevar(surl, :post_author)
+                width = rand(250:290)
+                pubdate = pagevar(surl, :published)
                 if isnothing(pubdate)
                     date    = "$ys-$ms-01"
                     days[i] = 1
@@ -32,74 +49,24 @@ function hfun_news()
                     date    = Date(pubdate, dateformat"d U Y")
                     days[i] = day(date)
                 end
-                lines[i] = "\n[$title]($url) $date \n"
+                lines[i] = """"""
+                lines[i] *= """<div class="news-block" style="width:$(width)px;margin:-5px;">"""
+                lines[i] *= """<a href="$url">$post_title</a> $date<br>"""
+                lines[i] *= """<p>$short_descr</p>"""
+                lines[i] *= """$post_author"""
+                for b in author_badges[post_author]
+                      lines[i] *= """<img src="/assets/$b.png"/>"""
+                end
+                lines[i] *= """</div>"""
             end
             # sort by day
-            foreach(
-                line -> write(io, line),
-                lines[sortperm(days, rev=true)]
-            )
+            foreach(line -> write(io, line), lines[sortperm(days, rev=false)])
         end
     end
-    md = String(take!(io))
-    return html(md, cur_lc(), nop=true)
-end
-
-
-"""
-    {{recentnews}}
-
-Input the 3 latest news posts.
-"""
-function hfun_recentnews()
-    curyear = Dates.Year(Dates.today()).value
-    ntofind = 3
-    nfound  = 0
-    recent  = Vector{Pair{String,Date}}(undef, ntofind)
-    for year in curyear:-1:2019
-        for month in 12:-1:1
-            ms = "0"^(1-div(month, 10)) * "$month"
-            base = joinpath("news", "$year", "$ms")
-            isdir(base) || continue
-            posts = filter!(p -> endswith(p, ".md"), readdir(base))
-            days  = zeros(Int, length(posts))
-            surls = Vector{String}(undef, length(posts))
-            for (i, post) in enumerate(posts)
-                ps       = splitext(post)[1]
-                surl     = "news/$year/$ms/$ps"
-                surls[i] = surl
-                pubdate  = getvarfrom(:published, surl, default=nothing)
-                days[i]  = isnothing(pubdate) ?
-                                1 : day(Date(pubdate, dateformat"d U Y"))
-            end
-            # go over month post in antichronological orders
-            sp = sortperm(days, rev=true)
-            for (i, surl) in enumerate(surls[sp])
-                recent[nfound + 1] = (surl => Date(year, month, days[sp[i]]))
-                nfound += 1
-                nfound == ntofind && break
-            end
-            nfound == ntofind && break
-        end
-        nfound == ntofind && break
-    end
-    resize!(recent, nfound)
-    io = IOBuffer()
-    for (surl, date) in recent
-        url   = "/$surl/"
-        title = getvarfrom(:title, surl, default="untitled")
-        sdate = "$(day(date)) $(monthname(date)) $(year(date))"
-        blurb = getvarfrom(:rss, surl)
-        write(io, """
-            <div class="col-lg-4 col-md-12 blog">
-              <h3><a href="$url" class="title" data-proofer-ignore>$title</a>
-              </h3><span class="article-date">$date</span>
-              <p>$blurb</p>
-            </div>
-            """)
-    end
+    write(io, """</div>""")
     return String(take!(io))
 end
+
 
 """
     {{ addcomments }}
